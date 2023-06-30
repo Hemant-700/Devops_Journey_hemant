1ee21238b1e17b6f0909a70a3c9d65b9b7355ce5
Hemant <127658426+Hemant-700@users.noreply.github.com>
2023-06-30 23:26:28 +0530

    Update jenkinsfile-terraform-s3.groovy

--- Jenkinsfiles/jenkinsfile-terraform-s3.groovy
+++ Jenkinsfiles/jenkinsfile-terraform-s3.groovy
@@ -2,24 +2,24 @@ pipeline {
     agent any
     stages {
         stage ('codecheckout') {
-            steps{
+            steps {
                 checkout scm
             }
         }
         stage ('Terraform') {
             steps {
                 dir ('/var/lib/jenkins/workspace/s3/Terraform/s3-new/') {
-                    withEnv (["Bucket_Name = ${params.Bucket_Name}"]) {
-                        sh'sudo terraform init'
+                    withEnv (["Bucket_Name=${params.Bucket_Name}"]) {
+                        sh 'sudo terraform init'
                         sleep(90)
-                        sh'sudo terraform apply -var "Bucket_Name = ${Bucket_Name}" -auto-approve'   
+                        sh 'sudo terraform apply -var "Bucket_Name=${env.Bucket_Name}" -auto-approve'   
                     }
                 }
             }
         }
         stage ('output') {
             steps {
-                echo "${Bucket_Name} has successfully created"
+                echo "${params.Bucket_Name} has been successfully created"
             }
         }
     }
